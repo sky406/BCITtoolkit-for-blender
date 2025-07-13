0ff521aeb9a4bfaae563ea241651b8cae77036e1
wesley <60670435+sky406@users.noreply.github.com>
2025-07-13 08:06:04 -0700

    intitial setup

---
+++ __init__.py
@@ -0,0 +1,13 @@
+import bpy 
+
+bl_info = {
+    "name":"BCIT tools",
+    "blender":(4,4,0),
+    "author":"wesley nworisa"
+}
+
+def register():
+    print("on")
+
+def unregister():
+    print("off")
