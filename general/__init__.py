694267f7d8fd55a8ded4378542ac18d2b7155589
sky406 <60670435+sky406@users.noreply.github.com>
2025-07-27 15:39:09 -0700

    added renaming tool, needs a lot of work

--- general/__init__.py
+++ general/__init__.py
@@ -1,12 +1,47 @@
 import bpy
 from .microwave import VIEW3DturnaroundPanel, turnAround
-# note for self do i really need a gneral panel probabbly not 
-subclasses=[turnAround,VIEW3DturnaroundPanel]
+from .rename import rename,renameSettings
+# the general panel is now the quick tools panel 
+
+class VIEW3D_BCITquicktools(bpy.types.Panel):
+    bl_idname = "VIEW3D_quicktools"
+    bl_label = "quicktools"
+    bl_category = "BCIT tools"
+    bl_region_type = "UI"
+    bl_space_type = "VIEW_3D"
+
+    def draw(self, context):
+        layout = self.layout
+        row  = layout.row()
+        row.operator(turnAround.bl_idname) 
+        # new row 
+        layout.separator()
+        # renaing tool
+        renamegroup = context.scene.additional_props
+        
+        column = layout.column()
+        column.prop(renamegroup,"prefix")
+        column.prop(renamegroup,"suffix")
+        column.prop(renamegroup,"replacement")
+        column.prop(renamegroup,"objType")
+        column.operator(rename.bl_idname)
+
+
+
+subclasses=[turnAround,renameSettings,rename]
+propertygroups = [renameSettings]
 def register():
     for subclass in subclasses:
         bpy.utils.register_class(subclass)
 
+    # register the property group
+    bpy.types.Scene.additional_props = bpy.props.PointerProperty(type=renameSettings)
+    bpy.utils.register_class(VIEW3D_BCITquicktools)
+
 def unregister():
     for subclass in subclasses:
+        print(subclass)
         bpy.utils.unregister_class(subclass)
+    del bpy.types.Scene.additional_props
+    bpy.utils.unregister_class(VIEW3D_BCITquicktools)
     
--- general/rename.py
+++ general/rename.py
@@ -1,2 +1,99 @@
 import bpy
-# TODO set up a renamer tool 
+class renameSettings(bpy.types.PropertyGroup):
+    """ this is essentially just a way to store my settings for the rename tool
+        its a bit jank so i'll come back to it one of these days
+      """
+    types = [
+        ("MESH", "mesh", "objects lmao"),
+        ("ARMATURE", "armature", "bones"),
+        ("CURVE", "curve", "(●'◡'●)"),
+        ("CAMERA", "camera", "its a camera my dude idk what to tell you"),
+        ("LIGHT", "lights", "lighting"),
+        
+    ]
+    
+
+    prefix: bpy.props.StringProperty(name="prefix",description="its a prefix")
+    suffix: bpy.props.StringProperty(name="suffix")
+    replacement: bpy.props.StringProperty(name="replace")
+    objType: bpy.props.EnumProperty(
+        name="object type",
+        items=types,
+        description="Select the object type to rename"
+    )
+
+    # for the sake of preventing prefix suffix stacking
+    lastPrefix: bpy.props.StringProperty(name="last prefix")
+    lastSuffix: bpy.props.StringProperty(name="last suffix")
+
+def renameByType():
+    """ 
+    type refers to blenders typing system. ie: "ARMATURE"
+    this more or less stacks the new name on the type
+    """
+    # grab data from the scene
+    renamesetting = bpy.context.scene.additional_props
+    type = renamesetting.objType
+    prefix = renamesetting.prefix
+    suffix = renamesetting.suffix
+    replaceWith = renamesetting.replacement
+    lastprefix = renamesetting.lastPrefix
+    lastsuffix = renamesetting.lastSuffix
+    # 
+    def fixName(name):
+        fixedname = name
+        if len(lastprefix)>0:
+            fixedname = fixedname[len(lastprefix):]
+    
+        if len(lastsuffix)>0:
+            fixedname = fixedname[:-len(lastsuffix)]
+        return fixedname
+    # 
+    sceneobjects = bpy.context.editable_objects
+    matchingTypes = []
+    for obj in sceneobjects:
+        if obj.type == type:matchingTypes.append(obj)
+
+    for obj in matchingTypes:
+        oldname = obj.name
+        oldname = fixName(oldname)
+        print(lastsuffix)
+        newname = ""
+        if prefix != "":
+            newname+=f"{prefix}_"
+            bpy.context.scene.additional_props.lastPrefix = f"{prefix}_"
+        
+        if replaceWith != "":
+            newname+=replaceWith
+        else:
+            newname+=oldname
+        
+        if suffix != "":
+            newname+=f"_{suffix}"
+            bpy.context.scene.additional_props.lastSuffix = f"_{suffix}"
+        obj.name = newname
+
+class rename(bpy.types.Operator):
+    bl_idname = "obj.rename"
+    bl_label = "rename"
+    bl_options = {"UNDO"}
+    bl_description = "renames all objects of a particular type"
+    
+  
+    def execute(self, context):
+        # small note this refers to rename settings when it is registered
+        # so this script only works when called within the quick tools panel
+        # renamesetting = context.scene.additional_props
+        # objtype = renamesetting.objType
+        # prefix = renamesetting.prefix
+        # suffix = renamesetting.suffix
+        # replacement = renamesetting.replacement
+        # print(f"{objtype}, {prefix}, {suffix}, {replacement}")
+        renameByType()
+        return {'FINISHED'}
+    
+ 
+
+
+    
+
